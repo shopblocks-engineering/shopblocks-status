5c5196deddccd648d269ffccd3f2790824b07707
Marcus Nightingale <marcusnightingale.1@gmail.com>
2021-12-14 18:34:40 +0000

    [Automated] Update Health Check Logs

--- health-check.sh
+++ health-check.sh
@@ -27,6 +27,7 @@ do
 
   for i in 1 2 3 4;
   do
+    echo $(curl --write-out '%{http_code}' --silent --output /dev/null $url)
     status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
     echo "$status_code"
     if [ "$status_code" -ne 200 ] || [ "$status_code" -eq 202 ] || [ "$status_code" -eq 301 ] || [ "$status_code" -eq 307 ]; then

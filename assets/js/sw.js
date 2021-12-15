4ad60014262d1c96a366c6534129a16a5e8c0930
Marcus Nightingale <marcusnightingale.1@gmail.com>
2021-12-15 00:43:38 +0000

    Adding some pwa stuff

---
+++ assets/js/sw.js
@@ -0,0 +1,33 @@
+const cachename = 'cache';
+const assets = [
+    '../assets/js/app.js',
+    '../assets/css/normalize.css',
+    '../assets/rocket.css',
+    '../assets/img/logo.ico',
+    '../assets/img/logo.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cachename).then(cache => {
+            cache.addAll(assets);
+        }).then(() => {
+            return self.skipWaiting();
+        })
+    )
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(caches.match(event.request)
+        .then(response => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    return self.clients.claim();
+});
